2135bc911a6e1e34cc305420dd895c0478476d5f
Chinolas <47531824+Ch1nolas@users.noreply.github.com>
2022-06-06 20:05:40 -0300

    Avance del proyecto

---
+++ mat.py
@@ -0,0 +1,16 @@
+#ejercicio: resolver la expresion algebraica
+#expreion: (x+y)*(x+y)= x^2 + 2xy + x^2 
+
+import math
+import os
+from re import X
+from tkinter import Y
+
+def evaluar_expresion(x, y):
+    return x**2+2*x*y+y**2
+X=float(input("escriba valor x aqui:"))
+Y=float(input("escriba valor x aqui:"))
+print:(evaluar_expresion(X, Y))
+
+
+
