0480d9e9d43cdf349bc970cc0b74bf3b2ffde219
Chinolas <47531824+Ch1nolas@users.noreply.github.com>
2022-06-11 16:19:06 -0300

    Actualización del proyecto Matemáticas

--- mat.py
+++ mat.py
@@ -1,16 +1,20 @@
-#ejercicio: resolver la expresion algebraica
-#expreion: (x+y)*(x+y)= x^2 + 2xy + x^2 
+import os, math
+#obtener la funcion exponencial, la cual esta dada por:
+#e^x = 1+x/1! + x²/2! + x³/3! + ...
 
-import math
-import os
-from re import X
-from tkinter import Y
-
-def evaluar_expresion(x, y):
-    return x**2+2*x*y+y**2
-X=float(input("escriba valor x aqui:"))
-Y=float(input("escriba valor x aqui:"))
-print:(evaluar_expresion(X, Y))
-
-
+ex = 0
+factorial = 0
+x = float (input ('Ingrese el valor de x: '))
+n = int (input ('Ingrese el valor de n: '))
+for i in range (1, n + 1):
+    print ('proceso ' + repr (i))
+    if i==1:
+        ex=1
+        factorial=1
+    factorial=factorial*i
+    ex=ex+math.pow(x,i)/factorial
+    print ()
+print ('Valor del exponente: ' + repr (ex))
+print ('Valor del factorial: ' + repr (factorial))
+os.system ('pause')
 
